3d416bfda492f7af974455c20ed0c17b95e96f20
ivankol <kolesar.ivan@gmail.com>
2014-01-22 19:11:30 +0100

    Variables renaming

--- UnityProject/Assets/Scripts/LSystem.cs
+++ UnityProject/Assets/Scripts/LSystem.cs
@@ -4,16 +4,19 @@ using System.Collections.Generic;
 
 public class LSystem : MonoBehaviour
 {	
-	public string   _axiom        = "A";
-	public string[] _stringRules  = {"A=(1.0)G", "F=(0.97)mG", "F=(0.03)mBG", "T=(1)[G]"};
-	public string   _moduleString = "";
+	public string   axiom                = "A";
+	public string[] lSystemRulesStr      = {"A=(1.0)G", "F=(0.97)mG", "F=(0.03)mBG", "T=(1)[G]"};
+	public string[] communicatorRulesStr = {"G=F", "B=T"};
+	public string   lSystemState         = "";
 
 	[HideInInspector] public List<String>     molecule_names;
 	[HideInInspector] public List<GameObject> molecule_objects;
 
 	private GameObject agentsSystem = null;
 
-	private Rules _rules = new Rules ();
+	private Rules                  lSystemRules      = new Rules ();
+	private Dictionary<char, char> communicatorRules = new Dictionary<char, char> ();
+
 	private List<Vector3> structureIdentification;
 	private bool changed = true;
 
@@ -37,37 +40,54 @@ public class LSystem : MonoBehaviour
 	
 	void Start ()
 	{
-		for (int i = 0; i < _stringRules.Length; i++)
-		{
-			Rule rule = Rule.Build (_stringRules[i]);
-			_rules.Add (rule);
-		}
-
-		_moduleString = _axiom;
+		lSystemState = axiom;
+		updateRules ();
 
 		structureIdentification = new List<Vector3> ();
 
 		agentsSystem = new GameObject("agentsSystem");
 		agentsSystem.AddComponent("AgentsSystem");
 	}
+
+	public void updateRules()
+	{
+		for (int i = 0; i < lSystemRulesStr.Length; i++)
+		{
+			Rule rule = Rule.Build (lSystemRulesStr[i]);
+			lSystemRules.Add (rule);
+		}
+
+		for (int i = 0; i < communicatorRulesStr.Length; i++)
+		{
+			string[] ruleSides = communicatorRulesStr[i].Split('=');
+
+			if (ruleSides.Length != 2)
+			{
+				// TODO: need some kind of warning
+				return;
+			}
+
+			communicatorRules.Add(ruleSides[0][0], ruleSides[1][0]);
+		}
+	}
 		
 	void Derive ()
 	{
-		string newModuleString = "";
-		for (int j = 0; j < _moduleString.Length; j++)
+		string newState = "";
+		for (int j = 0; j < lSystemState.Length; j++)
 		{
-			string module = _moduleString [j] + "";
+			string module = lSystemState [j] + "";
 			
-			if (!_rules.Contains (module))
+			if (!lSystemRules.Contains (module))
 			{
-				newModuleString += module;
+				newState += module;
 				continue;
 			}
 			
-			Rule rule = _rules.Get (module);
-			newModuleString += rule.successor;
+			Rule rule = lSystemRules.Get (module);
+			newState += rule.successor;
 		}
-		_moduleString = newModuleString;
+		lSystemState = newState;
 	}
 
 	void updateTurtle(ref Turtle turtle, GameObject gameObject, int bindingIndex)
@@ -105,9 +125,9 @@ public class LSystem : MonoBehaviour
 		Turtle current = new Turtle (Quaternion.identity, Vector3.zero);
 		Stack<Turtle> stack = new Stack<Turtle> ();
 
-		for (int i = 0; i < _moduleString.Length; i++)
+		for (int i = 0; i < lSystemState.Length; i++)
 		{
-			string module = _moduleString [i] + "";
+			string module = lSystemState [i] + "";
 
 			if (module == "m")
 			{
@@ -132,7 +152,6 @@ public class LSystem : MonoBehaviour
 			}
 			else if (module == "G")
 			{
-				// create something different
 				updateTurtle(ref current, molecule_objects[molecule_names.IndexOf("m")], 0);
 
 				agentsSystem.GetComponent<AgentsSystem>().createLocalSystem(i.ToString(), current.position, current.direction);
@@ -140,7 +159,6 @@ public class LSystem : MonoBehaviour
 			}
 			else if (module == "B")
 			{
-				// create something different
 				Turtle tempTurtle = new Turtle(current);
 				updateTurtle(ref tempTurtle, molecule_objects[molecule_names.IndexOf("m")], 1);
 
@@ -161,13 +179,10 @@ public class LSystem : MonoBehaviour
 				{
 					if(structureIdentification[j] == finishedBindings[i])
 					{
-						if (_moduleString[j] == 'G')
+						char symbol = lSystemState[j];
+						if (communicatorRules.ContainsKey(symbol))
 						{
-							_moduleString = ReplaceAtIndex(j, 'F', _moduleString);
-						}
-						else if (_moduleString[j] == 'B')
-						{
-							_moduleString = ReplaceAtIndex(j, 'T', _moduleString);
+							lSystemState = ReplaceAtIndex(j, communicatorRules[symbol], lSystemState);
 						}
 					}
 				}
