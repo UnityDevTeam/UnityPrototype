a0642437c5a26dded6fd0b4a2457426c28a61241
ivankol <kolesar.ivan@gmail.com>
2014-02-11 23:30:21 +0100

    Editor setting agents types and densities

---
+++ UnityProject/Assets/Editor/NewAgentSystemEditor.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+
+[CustomEditor(typeof(NewAgentSystem))]
+public class NewAgentSystemEditor : Editor
+{
+	private string[] allOptions = {"testAgent", "testAgent2"};
+	private int index = 0;
+
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector();
+		NewAgentSystem myAgentSystem = (NewAgentSystem)target;
+
+		List<string> selAgentsNames = myAgentSystem.agentTypesName;
+
+		List<string> options = new List<string> (allOptions);
+
+		EditorGUILayout.LabelField("Defined agent types : ");
+		for (int i = 0; i < selAgentsNames.Count; i++)
+		{
+			options.Remove(selAgentsNames[i]);
+
+			EditorGUILayout.BeginHorizontal ();
+
+			EditorGUILayout.LabelField(selAgentsNames[i], GUILayout.Width(100));
+			myAgentSystem.agentTypesDensity[i] = EditorGUILayout.Slider( myAgentSystem.agentTypesDensity[i], 0.00001f, 0.0001f );
+
+			if (GUILayout.Button ("Remove agent types"))
+			{
+				myAgentSystem.agentTypesName.RemoveAt(i);
+				myAgentSystem.agentTypesDensity.RemoveAt(i);
+			}
+
+			EditorGUILayout.EndHorizontal ();
+		}
+
+		if (options.Count != 0)
+		{
+			EditorGUILayout.BeginHorizontal ();
+
+			index = EditorGUILayout.Popup (index, options.ToArray ());
+			if (GUILayout.Button ("Add agent type"))
+			{
+				myAgentSystem.agentTypesName.Add (options [index]);
+				myAgentSystem.agentTypesDensity.Add(0.00001f);
+			}
+
+			EditorGUILayout.EndHorizontal ();
+		}
+	}
+}
--- UnityProject/Assets/Scripts/NewAgentSystem.cs
+++ UnityProject/Assets/Scripts/NewAgentSystem.cs
@@ -1,27 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class NewAgentSystem : MonoBehaviour
 {
 	public int agentsCount = 1000;
+
 	public Vector3 extentBox = new Vector3(100, 100, 100);
 	public Vector3 minBox = new Vector3(-50, -50, -50);
-	public string pdbPrefabs = "testAgent2";
+
+	[HideInInspector] public List<string> agentTypesName;
+	[HideInInspector] public List<float> agentTypesDensity;
 	
 	void Start ()
 	{
-		for (int i = 0; i < agentsCount; i++)
-		{
-			Vector3 position = new Vector3 ( Random.value * extentBox.x, Random.value * extentBox.y, Random.value * extentBox.z ) + minBox;
+		agentsCount = 0;
+		float agentSystemVolume = extentBox.x * extentBox.y * extentBox.z;
 
-			GameObject prefab = Resources.Load(pdbPrefabs) as GameObject;
-			GameObject mol = Instantiate(prefab, transform.position, transform.rotation) as GameObject;
-			mol.transform.parent = transform;
-			mol.transform.localPosition = position;
+		for (int i = 0; i < agentTypesName.Count; i++)
+		{
+			GameObject agentTypeObject = new GameObject(agentTypesName[i]);
+			agentTypeObject.transform.parent = transform;
+
+			int agentTypeCount = (int)(agentTypesDensity[i] * agentSystemVolume);
+
+			for (int j = 0; j < agentTypeCount; j++)
+			{
+				Vector3 position = new Vector3 ( Random.value * extentBox.x, Random.value * extentBox.y, Random.value * extentBox.z ) + minBox;
+				
+				GameObject prefab = Resources.Load(agentTypesName[i]) as GameObject;
+				GameObject mol = Instantiate(prefab, transform.position, transform.rotation) as GameObject;
+				mol.transform.parent = agentTypeObject.transform;
+				mol.transform.localPosition = position;
+			}
+
+			agentsCount += agentTypeCount;
 		}
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
 	
 	}
