750943b4fbdde05247cbbaf6721ec046b9f8c70c
xjules <julius.parulek@gmail.com>
2014-05-19 19:06:57 +0200

    pdb loader (fixed for one file)

--- UnityProject/Assets/Scripts/MainScript.cs
+++ UnityProject/Assets/Scripts/MainScript.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-using System.Collections;
+using System;
 
 public class MainScript : MonoBehaviour
 {
@@ -10,35 +10,7 @@ public class MainScript : MonoBehaviour
 	{
 
 	}
-
-	public void CreateMolContainer()
-	{
-		Vector3[] vertices = new Vector3[molCount];
-		int[] indices = new int[molCount];
-
-		for (var i=0; i < molCount; i++)
-		{
-			indices[i] = i;
-			vertices[i] = new Vector3 ( (Random.value - 0.5f), (Random.value - 0.5f), (Random.value - 0.5f));
-		}
-
-		GameObject molContainer = new GameObject("Mol Container");	
-
-		MeshRenderer meshRenderer = molContainer.AddComponent<MeshRenderer>();
-		meshRenderer.material = Resources.Load("MolMaterial") as Material;
-
-		MeshFilter meshFilter = molContainer.AddComponent<MeshFilter>();	
-		meshFilter.sharedMesh = new Mesh();
-		meshFilter.sharedMesh.vertices = vertices;
-		meshFilter.sharedMesh.SetIndices(indices, MeshTopology.Points, 0);
-
-//		material = Resources.Load("MolMaterial") as Material;
-//
-//		mesh = new Mesh();
-//		mesh.vertices = vertices;
-//		mesh.SetIndices(indices, MeshTopology.Points, 0);
-	}
-	
+		
 	// Update is called once per frame
 	void Update ()
 	{
